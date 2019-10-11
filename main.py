3fe342b1d63a37151c9ab3404985e7798a12d786
mac <markhomayzebe99@gmail.com>
2019-10-11 16:36:21 +0700

    Kecepatan tanpa fungsi

---
+++ main.py
@@ -0,0 +1,18 @@
+nama = 'Ulung Markho Mayzebe'
+program = 'Gerak Lurus'
+
+print(f'Program {program} oleh {nama}')
+
+jarak = 1000
+waktu = 5 * 60
+kecepatan = jarak / waktu
+
+print(f'jarak = {jarak/1000}km ditempuh dalam waktu = {waktu/60}menit')
+print(f'Sehingga kecepatan = {kecepatan} m/s')
+
+jarak = 5000
+waktu = 5 * 60
+kecepatan = jarak / waktu
+
+print(f'jarak = {jarak/1000}km ditempuh dalam waktu = {waktu/60}menit')
+print(f'Sehingga kecepatan = {kecepatan} m/s')
